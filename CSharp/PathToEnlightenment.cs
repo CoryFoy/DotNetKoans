d9bf56c62ccaf104fdd562c33312ad73a792b4c0
Chris Missal <chris.missal@gmail.com>
2009-12-23 23:20:26 -0600

    removed unnecessary usings

--- CSharp/PathToEnlightenment.cs
+++ CSharp/PathToEnlightenment.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace DotNetKoans.CSharp
+﻿namespace DotNetKoans.CSharp
 {
     public class PathToEnlightenment : KoanHelpers.IAmThePathToEnlightenment
     {
